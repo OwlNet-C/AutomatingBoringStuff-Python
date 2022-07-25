3e56b418a18c5706f36571f0079d595dd33e9b01
OwlNet-C <45668467+OwlNet-C@users.noreply.github.com>
2022-07-25 02:53:41 +0100

    Create Birthdays.py

---
+++ Chapter5/Birthdays.py
@@ -0,0 +1,16 @@
+birthdays = {'Milot':'Mar 29', 'Joy':'Oct 6', 'Nay': '22 Nov' }
+
+while True:
+    print('Enter a name: (blank to quit)')
+    name = input()
+    if name == '':
+        break
+
+    if name in birthdays:
+        print(birthdays[name] + ' is the birthday of ' + name)
+    else:
+        print('I do not have birthday information for ' + name)
+        print('What is their birthday?')
+        bday = input()
+        birthdays[name] = bday
+        print('Birthday database updated.')
